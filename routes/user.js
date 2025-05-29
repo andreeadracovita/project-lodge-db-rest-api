efd661b8f7b7eb0092f868b630449d1297d94e3b
Andreea Racovita <andreearacovitagit@gmail.com>
2025-05-29 13:42:38 +0300

    Pipe user details from DB

--- routes/user.js
+++ routes/user.js
@@ -3,8 +3,26 @@ import db from "../db/db.js";
 
 const router = express.Router();
 
-router.get("/config", (req, res) => {
-	res.json({ img_url: "00000001.jpg", currency: "CHF", language: "de-CH" });
+router.get("/config", async (req, res) => {
+	try {
+		const result = await db.query("SELECT * FROM user_details WHERE user_id = $1", [
+			req.user.id
+		]);
+		if (result.rows.length > 0) {
+			const userDetails = result.rows[0];
+			return res.json({
+				first_name: userDetails.first_name,
+				img_url: userDetails.img_url,
+				language: userDetails.language,
+				currency: userDetails.currency,
+				experiences_ids: userDetails.experiences_ids
+			});
+		} else {
+			return res.status(404).send("User not found");
+		}
+	} catch (error) {
+		console.log(error);
+	}
 });
 
 export default router;
