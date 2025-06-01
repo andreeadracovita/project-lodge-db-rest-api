f1b44f0ba97d91f7905940b2a5300c261e5ba48b
Andreea Racovita <andreearacovitagit@gmail.com>
2025-06-01 12:18:25 +0300

    Add GET /user/properties route

--- routes/user.js
+++ routes/user.js
@@ -3,6 +3,7 @@ import db from "../db/db.js";
 
 const router = express.Router();
 
+// GET /user/config
 router.get("/config", async (req, res) => {
 	try {
 		const result = await db.query("SELECT * FROM user_details WHERE user_id = $1", [
@@ -22,6 +23,17 @@ router.get("/config", async (req, res) => {
 		}
 	} catch (error) {
 		console.log(error);
+	}
+});
+
+// GET /user/properties
+router.get("/properties", async (req, res) => {
+	const userId = parseInt(req.user.id);
+	try {
+		const result = await db.query("SELECT p.id, p.title, p.geo, p.city, p.country, p.is_listed, pd.rating, pd.images_url_array, pd.price_per_night_eur AS price FROM properties AS p, property_details AS pd WHERE p.id = pd.property_id AND pd.host_id = $1", [userId]);
+		res.json(result.rows);
+	} catch (err) {
+		console.log(err);
 	}
 });
 
