69a2311f2064214c1e7312ec32f1a8bafc2e3e76
Andreea Racovita <andreearacovitagit@gmail.com>
2025-08-25 15:44:14 +0300

    [DB] SSL settings

--- db/db.js
+++ db/db.js
@@ -9,7 +9,10 @@ const db = new pg.Client({
 	host: process.env.PG_HOST,
 	database: process.env.PG_DATABASE,
 	password: process.env.PG_PASSWORD,
-	port: process.env.PG_PORT
+	port: process.env.PG_PORT,
+	ssl: {
+		rejectUnauthorized: false,
+	}
 });
 
 db.connect();
